5c8df97efdb2295fb2acebf68bd53b89c80eba3d
rOmAn2193 <roman_puzey@aol.com>
2016-07-01 11:40:03 -0600

    BMI Calculator
    Calculates your BMI in pounds and kilograms

---
+++ BMICalculator/ViewController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+using UIKit;
+
+namespace BMICalculator
+{
+	public partial class ViewController : UIViewController
+	{
+		public ViewController (IntPtr handle) : base (handle)
+		{
+		}
+
+		public override void ViewDidLoad ()
+		{
+			base.ViewDidLoad ();
+			// Perform any additional setup after loading the view, typically from a nib.
+
+			calculateButton.TouchUpInside += CalculateButton_TouchUpInside;
+		}
+
+		void CalculateButton_TouchUpInside (object sender, EventArgs e)
+		{
+			float height = float.Parse(heightTextField.Text);
+			float weight = float.Parse (weightTextField.Text);
+
+			float bmi = (weight / (height * height));
+
+			bmiLabel.Text = bmi.ToString ();
+		}
+
+		public override void DidReceiveMemoryWarning ()
+		{
+			base.DidReceiveMemoryWarning ();
+			// Release any cached data, images, etc that aren't in use.
+		}
+	}
+}
+
